351d2014f6dee43941cfbafd77130ba015edf8fc
Keijiro Takahashi <keijiro@Super-MSX.local>
2013-11-28 10:42:08 +0900

    Refined physics rig.

--- Assets/Scripts/HeadMovement.cs
+++ Assets/Scripts/HeadMovement.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class HeadMovement : MonoBehaviour
 {
+    public float speed = 3.0f;
+    public float radius = 0.4f;
     float r1;
     float r2;
     float vr1;
@@ -10,15 +12,15 @@ public class HeadMovement : MonoBehaviour
 
     void Start ()
     {
-        vr1 = Random.Range (0.1f, 0.5f);
-        vr2 = Random.Range (0.1f, 0.5f);
+        vr1 = Random.Range (0.1f, 0.5f) * speed;
+        vr2 = Random.Range (0.1f, 0.5f) * speed;
     }
     
     void FixedUpdate ()
     {
         r1 += vr1 * Time.fixedDeltaTime;
         r2 += vr2 * Time.fixedDeltaTime;
-        var position = new Vector3 (Mathf.Sin (r1), transform.position.y, Mathf.Sin (r2));
+        var position = new Vector3 (Mathf.Sin (r1) * radius, transform.position.y, Mathf.Sin (r2) * radius);
         rigidbody.MovePosition (position);
     }
 }
---
+++ Assets/Scripts/MobileBuilder.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MobileBuilder : MonoBehaviour
+{
+    public GameObject barPrefab;
+    public int iteration = 10;
+    public float interval = 0.25f;
+    public float pivot = 0.8f;
+
+    void Start ()
+    {
+        var position = transform.position;
+        var rotation = transform.rotation;
+        var prev = gameObject;
+
+        for (var i = 0; i < iteration; i++)
+        {
+            position -= Vector3.up * interval;
+
+            var bar = Instantiate(barPrefab, position, rotation) as GameObject;
+
+#if false
+            var joint = bar.AddComponent<HingeJoint> ();
+            joint.anchor = position + Vector3.right * pivot;
+            joint.axis = Vector3.up;
+            joint.connectedBody = prev.rigidbody;
+#else
+            var joint = bar.AddComponent<ConfigurableJoint> ();
+
+            joint.anchor = position + Vector3.right * pivot;
+            joint.axis = Vector3.up;
+            joint.connectedBody = prev.rigidbody;
+
+            joint.angularXMotion = ConfigurableJointMotion.Free;
+            joint.angularYMotion = ConfigurableJointMotion.Locked;
+            joint.angularZMotion = ConfigurableJointMotion.Locked;
+
+            joint.xMotion = ConfigurableJointMotion.Locked;
+            joint.yMotion = ConfigurableJointMotion.Locked;
+            joint.zMotion = ConfigurableJointMotion.Locked;
+#endif
+
+            prev = bar;
+            pivot *= -1.0f;
+        }
+    }
+}
